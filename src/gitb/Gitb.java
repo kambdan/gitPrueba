6186d576db1fc8341ce7ae862d193cd711711772
Juan C <juancambi1319@gmail.com>
2020-01-02 11:49:44 -0500

    mi primer commit

---
+++ src/gitb/Gitb.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitb;
+
+/**
+ *
+ * @author STRIX
+ */
+public class Gitb {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
